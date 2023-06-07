8c7a29e8191288aa89588275f9eb2a82122d23b5
EvaPelko <75135057+EvaPelko@users.noreply.github.com>
2023-06-07 19:55:51 +0200

    text field updatean

--- src/views/PrviZadatak.vue
+++ src/views/PrviZadatak.vue
@@ -5,16 +5,25 @@
 				<v-card class="pa-3" outlined width="600px">
 					<v-card-title>Dodaj studenta</v-card-title>
 					<v-card-text>
-						<v-form @submit="dodajStudenta">
-							<input type="text" placeholder="Ime" v-model="ime"> <br><br>
-							<input type="text" placeholder="Prezime" v-model="prezime"> <br><br>
-							<input type="number" placeholder="Broj dolazaka (max 15)" v-model="brojDolazaka"> <br> <br>
-							<input type="number" placeholder="Rezultat 1. kolokvija (max 40)" v-model="broj1Kolokvij">
-							<br> <br>
-							<input type="number" placeholder="Rezultat 2. kolokvija (max 40)" v-model="broj2Kolokvij">
-							<br> <br>
-							<input type="number" placeholder="Pracenje (max 20)" v-model="brojPracenje">
-							<br> <br>
+						<v-form @submit="dodajStudenta" lazy-validation>
+							<v-text-field type="text" placeholder="Ime" v-model="ime" :rules="nameRules" label="ime"
+								required>
+							</v-text-field>
+							<v-text-field type="text" placeholder="Prezime" v-model="prezime" :rules="nameRules"
+								label="prezime" required>
+							</v-text-field>
+							<v-text-field type="number" placeholder="Broj dolazaka (max 15)" v-model="brojDolazaka"
+								:rules="nameRules" label="brojDolazaka" maxlength="15" required>
+							</v-text-field>
+							<v-text-field type="number" placeholder="Rezultat 1. kolokvija (max 40)" v-model="broj1Kolokvij"
+								:rules="nameRules" label="broj1Kolokvij" maxlength="40" required>
+							</v-text-field>
+							<v-text-field type="number" placeholder="Rezultat 2. kolokvija (max 40)" v-model="broj2Kolokvij"
+								:rules="nameRules" label="broj2Kolokvij" maxlength="40" required>
+							</v-text-field>
+							<v-text-field type="number" placeholder="Pracenje (max 20)" v-model="brojPracenje"
+								:rules="nameRules" label="brojPracenje" maxlength="20" required>
+							</v-text-field>
 
 						</v-form>
 					</v-card-text>
@@ -23,8 +32,8 @@
 							BRISI PODATKE
 						</v-btn>
 						<v-spacer></v-spacer>
-						<v-btn color="red">OČISTI</v-btn>
-						<v-btn type="submit" @click="dodajStudenta">OK</v-btn>
+						<v-btn color="red" @click="ocistiFormu">OČISTI</v-btn>
+						<v-btn @click="dodajStudenta">OK</v-btn>
 					</v-card-actions>
 				</v-card>
 			</v-col>
@@ -48,6 +57,7 @@ export default {
 	},
 	watch: {},
 	methods: {
+
 		ocistiFormu() { },
 		dodajStudenta() {
 			let noviStudent = {
@@ -65,19 +75,18 @@ export default {
 			}
 			studenti.push(noviStudent);
 			localStorage.setItem("studenti", JSON.stringify(studenti));
-
-			if (this.ime && this.prezime && this.brojDolazaka && this.broj1Kolokvij && this.broj2Kolokvij && this.brojPracenje) {
-				console.log("login function called")
-			}
-			this.error = [];
-			if (this.ime) {
-				this.error.push("name is required")
-			}
-			if (this.prezime) {
-				this.error.push("prezime is required")
-			}
-			console.ward("errors", this.error)
-			e.preventDefault();
+			/*
+						if (this.ime && this.prezime && this.brojDolazaka && this.broj1Kolokvij && this.broj2Kolokvij && this.brojPracenje) {
+							console.log("login function called")
+						}
+						this.error = [];
+						if (this.ime) {
+							this.error.push("name is required")
+						}
+						if (this.prezime) {
+							this.error.push("prezime is required")
+						}
+			*/
 		},
 		obrisiSveUnesenePodatke() {
 			localStorage.clear();
