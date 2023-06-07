19e683d1b11bd414b8f4c4baedf4f000910c4fe9
EvaPelko <75135057+EvaPelko@users.noreply.github.com>
2023-06-07 19:19:59 +0200

    init

---
+++ src/views/PrviZadatak.vue
@@ -0,0 +1,69 @@
+<template>
+	<v-container fill-height fluid class="background">
+		<v-row>
+			<v-col cols="12">
+				<v-card class="pa-3" outlined width="600px">
+					<v-card-title>Dodaj studenta</v-card-title>
+					<v-card-text>
+						<v-form @submit="dodajStudenta">
+							<input type="text" placeholder="Ime" v-model="ime"> <br><br>
+							<input type="text" placeholder="Prezime" v-model="prezime"> <br><br>
+							<input type="number" placeholder="Broj dolazaka (max 15)" v-model="brojDolazaka"> <br> <br>
+							<input type="number" placeholder="Rezultat 1. kolokvija (max 40)" v-model="broj1Kolokvij">
+							<br> <br>
+							<input type="number" placeholder="Rezultat 2. kolokvija (max 40)" v-model="broj2Kolokvij">
+							<br> <br>
+							<input type="number" placeholder="Pracenje (max 20)" v-model="brojPracenje">
+							<br> <br>
+
+						</v-form>
+					</v-card-text>
+					<v-card-actions>
+						<v-btn color="black" class="white--text" elevation="0" @click="obrisiSveUnesenePodatke">
+							BRISI PODATKE
+						</v-btn>
+						<v-spacer></v-spacer>
+						<v-btn color="red">OČISTI</v-btn>
+						<v-btn>OK</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+export default {
+	name: "prvi-zadatak",
+	data() {
+		return {
+			error: [],
+			ime: null,
+			prezime: null,
+			brojDolazaka: null,
+			broj1Kolokvij: null,
+			broj2Kolokvij: null,
+			brojPracenje: null,
+		};
+	},
+	watch: {},
+	methods: {
+		ocistiFormu() { },
+		dodajStudenta() {
+			let noviStudent = {
+				//Dodaj propertyje
+			};
+			//ovo ne diraj
+			let studenti = JSON.parse(localStorage.getItem("studenti"));
+			if (!studenti) {
+				studenti = [];
+			}
+			studenti.push(noviStudent);
+			localStorage.setItem("studenti", JSON.stringify(studenti));
+		},
+		obrisiSveUnesenePodatke() {
+			localStorage.clear();
+		},
+	},
+};
+</script>
